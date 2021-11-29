6d3393677d1c1a8863ee266d317cc9cde4cd0653
André Maciel <56964039+macielandre@users.noreply.github.com>
2021-11-29 02:07:53 -0300

    Help option added

--- bot.js
+++ bot.js
@@ -10,13 +10,16 @@ client.on('messageCreate', async message=> {
     const command = message_string.replace('!', '')
 
     if(command === 'ping'){
-        temporary_message = await message.channel.send('O ping do bot é de 000 ms!')
-        temporary_message.edit(`O ping do bot é de ${temporary_message.createdTimestamp - message.createdTimestamp} ms!`)
+        temporary_message = await message.channel.send('Bot ping is now 000 ms!')
+        temporary_message.edit(`Bot ping is now ${temporary_message.createdTimestamp - message.createdTimestamp} ms!`)
     }
+    
+    else if(command === 'help')
+        await message.channel.send('!help: Bot help\n!play: Play tracks or playlists\n!pause: Pause track\n!skip: Skip track\n!queue: Tracks queue\n!clear: Clear queue\n!voteskip: Vote to skip\n!ping: Bot ping\n!disconect: Disconect bot')
 })
 
 client.once('ready', () => {
-    console.log(`> Starting bot dependencies\n> Id: ${client.user.id}\n> Logged in as ${client.user.tag}`)
+    console.log(`[bot] starting bot dependencies\n[bot] id: ${client.user.id}\n[bot] logged in as ${client.user.tag}`)
 })
 
 client.login(config.token)
